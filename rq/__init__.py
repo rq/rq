a491ff248d2195e3ef72bc39e0d9e834005d62ba
Vincent Driessen <vincent@3rdcloud.com>
2011-11-17 13:29:32 +0100

    Add simple helper function, to avoid users to learn about the `conn` stack.
    This makes it easier to start using the product.

--- rq/__init__.py
+++ rq/__init__.py
@@ -1,5 +1,13 @@
+from redis import Redis
 from .proxy import conn
 from .queue import Queue
 from .worker import Worker
 
-__all__ = ['conn', 'Queue', 'Worker']
+def use_redis(redis=None):
+    if redis is None:
+        redis = Redis()
+    elif not isinstance(redis, Redis):
+        raise TypeError('Argument redis should be a Redis instance.')
+    conn.push(redis)
+
+__all__ = ['conn', 'Queue', 'Worker', 'use_redis']
