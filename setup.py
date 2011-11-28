c820ba24faa004346a450867e17b0f428e44c59a
Vincent Driessen <vincent@3rdcloud.com>
2011-11-28 12:11:09 +0100

    Move to Alpha stage.

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         #'Development Status :: 1 - Planning',
-        'Development Status :: 2 - Pre-Alpha',
-        #'Development Status :: 3 - Alpha',
+        #'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         #'Development Status :: 4 - Beta',
         #'Development Status :: 5 - Production/Stable',
         #'Development Status :: 6 - Mature',
