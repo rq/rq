9ac9c234126cddece2fb92a4b419d6a9c16624c7
Vincent Driessen <vincent@3rdcloud.com>
2012-02-22 17:39:14 +0100

    Flake8.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -3,19 +3,23 @@ This file contains all jobs that are used in tests.  Each of these test
 fixtures has a slighty different characteristics.
 """
 
+
 def say_hello(name=None):
     """A job with a single argument and a return value."""
     if name is None:
         name = 'Stranger'
     return 'Hi there, %s!' % (name,)
 
+
 def do_nothing():
     """The best job in the world."""
     pass
 
+
 def div_by_zero(x):
     """Prepare for a division-by-zero exception."""
     return x / 0
+
 
 def some_calculation(x, y, z=1):
     """Some arbitrary calculation with three numbers.  Choose z smartly if you
@@ -23,10 +27,9 @@ def some_calculation(x, y, z=1):
     """
     return x * y / z
 
+
 def create_file(path):
     """Creates a file at the given path.  Actually, leaves evidence that the
     job ran."""
     with open(path, 'w') as f:
         f.write('Just a sentinel.')
-
-
