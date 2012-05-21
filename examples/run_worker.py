48cee215afb92acb10c6260b39017890e21f70c9
Vincent Driessen <vincent@3rdcloud.com>
2012-05-21 08:08:59 +0200

    Rewrite of the connection setup.
    I'd want to advice against using `use_connection()` more, as the
    connection setup is less explicit, and also pollutes global memory.
    
    Because, well:
    
        $ python -m this | head -n4 | tail -n1
    
    ;)

--- examples/run_worker.py
+++ examples/run_worker.py
@@ -1,8 +1,8 @@
-from rq import Queue, Worker, use_connection
+from rq import Queue, Worker, Connection
 
-# Tell rq what Redis connection to use
-use_connection()
 
 if __name__ == '__main__':
-    q = Queue()
-    Worker(q).work()
+    # Tell rq what Redis connection to use
+    with Connection():
+        q = Queue()
+        Worker(q).work()
