7cc586080e1a147674ecb64fa4f0f6cf40832ed6
Jeffrey Gelens <jeffrey@gelens.org>
2012-05-24 23:06:50 +0200

    Moved example Gevent runner to a new file.

--- examples/run_gevent_worker.py
+++ examples/run_gevent_worker.py
@@ -1,9 +1,9 @@
 from rq import Queue, use_connection
-from rq.worker import Worker
+from rq.worker import GeventWorker as Worker
 
 # Tell rq what Redis connection to use
 use_connection()
 
 if __name__ == '__main__':
     q = Queue()
-    Worker(q).work()
+    Worker(q, slaves=8).work()
