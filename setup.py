a3fb6a02a2c039fe53326ab7bf974efa1df0c2fe
Selwin Ong <selwin.ong@gmail.com>
2012-06-17 15:35:21 +0700

    Bump version to fix pypi packaging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rq-scheduler',
-    version='0.1',
+    version='0.1.1',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['rq_scheduler'],
