dcb5e46cd5cfd9aeb21c607c3e9e3ec102d125ff
Matt Ball <yosemitebandit@gmail.com>
2012-06-25 11:07:04 -0700

    adds a switch for changing the sleeping interval of the scheduler script

--- rq_scheduler/scripts/rqscheduler.py
+++ rq_scheduler/scripts/rqscheduler.py
@@ -12,9 +12,12 @@ def main():
     parser.add_argument('-p', '--port', default=6379, type=int, help="Redis port number")
     parser.add_argument('-d', '--db', default=0, type=int, help="Redis database")
     parser.add_argument('-P', '--password', default=None, help="Redis password")
+    parser.add_argument('-i', '--interval', default=60, type=int
+        , help="How often the scheduler checks for new jobs to add to the \
+            queue (in seconds).")
     args = parser.parse_args()
     connection = Redis(args.host, args.port, args.db, args.password)
-    scheduler = Scheduler(connection=connection)
+    scheduler = Scheduler(connection=connection, interval=args.interval)
     scheduler.run()
 
 if __name__ == '__main__':
