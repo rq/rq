635107605a634d405176bc13f9746d4bfee98932
Selwin Ong <selwin.ong@gmail.com>
2012-07-20 14:26:39 +0700

    Bump version number

--- rq_scheduler/__init__.py
+++ rq_scheduler/__init__.py
@@ -1,3 +1,3 @@
-VERSION = (0, 2, 0)
+VERSION = (0, 3, 0)
 
 from .scheduler import Scheduler
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='rq-scheduler',
-    version='0.2.0',
+    version='0.3.0',
     author='Selwin Ong',
     author_email='selwin.ong@gmail.com',
     packages=['rq_scheduler'],
