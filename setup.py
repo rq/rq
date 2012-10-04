eb00f69cedea624cee5bf91eccde963cc1c35956
Jon Parise <jon@indelible.org>
2012-10-04 16:57:49 -0700

    Add a standard --url argument to all scripts.
    redis-py now supports URL-based connection configuration.  When --url
    is specified, we use it to construct the Redis object.  Otherwise, we
    use the existing argument-based construction method.
    
    `Redis.from_url()` is new in redis-py 2.6.2, so that prerequisite has
    been adjusted accordingly.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def get_version():
 
 
 def get_dependencies():
-    deps = ['redis >= 2.4.0', 'times']
+    deps = ['redis >= 2.6.2', 'times']
     deps += ['logbook']  # should be soft dependency?
     if sys.version_info < (2, 7) or \
             (sys.version_info >= (3, 0) and sys.version_info < (3, 1)):
