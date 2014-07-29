3dc008d090a5cb814524eddbe09f27829d034fb0
Selwin Ong <selwin.ong@gmail.com>
2014-07-29 18:26:24 +0700

    Added WorkingQueue class.

---
+++ tests/test_working_queue.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+from rq.job import Job
+from rq.utils import current_timestamp
+from rq.working_queue import WorkingQueue
+
+from tests import RQTestCase
+
+
+class TestQueue(RQTestCase):
+
+    def setUp(self):
+        super(TestQueue, self).setUp()
+        self.working_queue = WorkingQueue('default', connection=self.testconn)
+
+    def test_add_and_remove(self):
+        """Adding and removing job to WorkingQueue."""
+        timestamp = current_timestamp()
+        job = Job()
+
+        # Test that job is added with the right score
+        self.working_queue.add(job, 1000)
+        self.assertLess(self.testconn.zscore(self.working_queue.key, job.id),
+                        timestamp + 1001)
+
+        # Ensure that job is properly removed from sorted set
+        self.working_queue.remove(job)
+        self.assertIsNone(self.testconn.zscore(self.working_queue.key, job.id))
+
+    def test_get_job_ids(self):
+        """Getting job ids from WorkingQueue."""
+        self.testconn.zadd(self.working_queue.key, 1, 'foo')
+        self.testconn.zadd(self.working_queue.key, 10, 'bar')
+        self.assertEqual(self.working_queue.get_job_ids(), ['foo', 'bar'])
+
+    def test_get_expired_job_ids(self):
+        """Getting expired job ids form WorkingQueue."""
+        timestamp = current_timestamp()
+
+        self.testconn.zadd(self.working_queue.key, 1, 'foo')
+        self.testconn.zadd(self.working_queue.key, timestamp + 10, 'bar')
+
+        self.assertEqual(self.working_queue.get_expired_job_ids(), ['foo'])
