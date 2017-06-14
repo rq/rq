3aececa15369807076aabeb133ab8feadb1cc088
Dharmendra Verma <dkverma.vit@gmail.com>
2017-06-14 19:21:43 +0530

    job_id as env support

--- rq/defaults.py
+++ rq/defaults.py
@@ -6,6 +6,6 @@ DEFAULT_WORKER_CLASS = 'rq.Worker'
 DEFAULT_CONNECTION_CLASS = 'redis.StrictRedis'
 DEFAULT_WORKER_TTL = 420
 DEFAULT_RESULT_TTL = 500
-namespace = os.getenv('RQ_NAMESPACE')
+namespace = os.getenv('RQ_NAMESPACE') or os.getenv('JOB_ID')
 DEFAULT_NAMESPACE = ":{}".format(namespace) if namespace else ''
 DEFAULT_QUEUE_TIMEOUT = int(os.getenv('RQ_QUEUE_TIMEOUT', 180))
