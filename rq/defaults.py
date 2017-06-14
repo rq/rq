7a99f928240d0cd069a5e9553860b2591a642bc8
Dharmendra Verma <dkverma.vit@gmail.com>
2017-06-14 19:19:15 +0530

    RQ_QUEUE_TIMEOUT

--- rq/defaults.py
+++ rq/defaults.py
@@ -8,4 +8,4 @@ DEFAULT_WORKER_TTL = 420
 DEFAULT_RESULT_TTL = 500
 namespace = os.getenv('RQ_NAMESPACE')
 DEFAULT_NAMESPACE = ":{}".format(namespace) if namespace else ''
-DEFAULT_QUEUE_TIMEOUT = int(os.getenv('DEFAULT_QUEUE_TIMEOUT', 180))
+DEFAULT_QUEUE_TIMEOUT = int(os.getenv('RQ_QUEUE_TIMEOUT', 180))
