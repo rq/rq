44c4c00dbeb1308e84c807b9f4347882321f220c
Dharmendra Verma <dkverma.vit@gmail.com>
2017-06-15 18:30:28 +0530

    load env changed

--- rq/__init__.py
+++ rq/__init__.py
@@ -3,6 +3,15 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+from os.path import exists
+
+from dotenv import load_dotenv
+
+dotenv_path = '/etc/environment'
+if exists(dotenv_path):
+    print("loading environment variables from /etc/environment file..")
+    load_dotenv(dotenv_path)
+
 from .connections import (Connection, get_current_connection, pop_connection,
                           push_connection, use_connection)
 from .job import cancel_job, get_current_job, requeue_job
