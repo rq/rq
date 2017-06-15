71e74d9e05f9721554bdc07102018943a4e3f7b3
Dharmendra Verma <dkverma.vit@gmail.com>
2017-06-15 17:40:10 +0530

    laod env

--- rq/cli/__init__.py
+++ rq/cli/__init__.py
@@ -1,8 +1,8 @@
-from os.path import join, dirname, exists
+from os.path import exists
 
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '/etc/environment')
+dotenv_path = '/etc/environment'
 if exists(dotenv_path):
     print("loading environment variables from /etc/environment file..")
     load_dotenv(dotenv_path)
