0fab93d683f7a8d6e8ad11a60095baf5e0261a5c
Theo <theo.despoudis@teckro.com>
2017-09-08 12:21:30 +0100

    Fixed #809 - Added tests for various cli config parameters

---
+++ tests/dummy_settings_override.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+REDIS_HOST = "testhost.example.com"
+REDIS_PORT = 6378
+REDIS_DB = 2
+REDIS_PASSWORD = '123'
