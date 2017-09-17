32aef302bda0b7e8c0dc0a9e818443792cab9dea
trapito <l_alminana@yahoo.com.ar>
2017-09-17 01:22:38 -0300

    Completely removed pywin32 as a dependency replacing process creation with spanve which means _monitor_work_horse_tick does not need to be overriden
    Moved the task timeout logic to the WindowsDeathPenalty class, now instead of verifying it in the parent and killing the child the checks are performed by the child with the following business logic :
    
      A secondadry thread keeps running which causes a KeyboardInterrupt in the main thread if the jobs timeouts
      A winsock blocking call hook terminates blocking calls if that's the reason the main thread is locked
    
      Note 1 : synchronization between those routines is very important and the Event object is key to prevent a race condition which would end up with the process not being interrupted
      Note 2 : if those mechanisms fail and the job execution time exceeds timeout * 1.5 the secondary task will end the process with error return code 255 (we might want to change this)
    
    Added a windows colorizer manager that returns the text as is to avoid control character clutter

--- rq/timeouts.py
+++ rq/timeouts.py
@@ -2,7 +2,16 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import os
+import time
+import ctypes
+import socket
 import signal
+
+try:
+    import _thread
+except ImportError as ex:
+    import thread as _thread
 
 
 class JobTimeoutException(Exception):
@@ -67,11 +76,91 @@ class UnixSignalDeathPenalty(BaseDeathPenalty):
 
 
 class WindowsDeathPenalty(BaseDeathPenalty):
+    ws2_32 = ctypes.windll.ws2_32
+    wsock32 = ctypes.windll.wsock32
+    kernel32 = ctypes.windll.kernel32
+
+    WSAIsBlocking = wsock32.WSAIsBlocking
+    WSASetBlockingHook = ws2_32.WSASetBlockingHook
+    WSAUnhookBlockingHook = ws2_32.WSAUnhookBlockingHook
+    WSACancelBlockingCall = wsock32.WSACancelBlockingCall
+
+    SetEvent = kernel32.SetEvent
+    CloseHandle = kernel32.CloseHandle
+    CreateEventW = kernel32.CreateEventW
+    WaitForSingleObject = kernel32.WaitForSingleObject
+
+    def thread_timeout_watchdog(self):
+        while self.wait_for_timeout:
+            current_time = time.time()
+
+            if current_time >= self.time_limit:
+                if current_time - self.time_limit > (self._timeout * 1.5):
+                    # Print an error to report the forceful exit
+                    os._exit(255)
+
+                else:
+                    with self.watchdog_lock:
+                        self.timeout_detected = True
+                        self.wait_for_timeout = False
+
+                        _thread.interrupt_main()
+
+            self.WaitForSingleObject(self.timeout_event, 500)
+
+        return 0
+
+    def socket_timeout_watchdog(self):
+        current_time = time.time()
+
+        if current_time >= self.time_limit:
+            with self.watchdog_lock:
+                blocking = self.WSAIsBlocking()
+
+                if blocking:
+                    self.SetEvent(self.timeout_event)
+                    self.WSACancelBlockingCall()
+
+        return 0
+
     def handle_death_penalty(self, signum, frame):
-        pass
+        if self.timeout_detected:
+            raise JobTimeoutException('Job exceeded maximum timeout '
+                                      'value ({0} seconds)'.format(self._timeout))
+
+        else:
+            self._original_sigint_handler(signum, frame)
 
     def setup_death_penalty(self):
-        pass
+        self.timeout_detected = False
+        self.wait_for_timeout = True
+        self.watchdog_lock = _thread.allocate_lock()
+        self.timeout_event = self.CreateEventW(None, 1, 0, None)
+        self.time_limit = time.time() + self._timeout
+
+        # force winsock to be initialized
+
+        dummy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        dummy_socket.close()
+
+        # break free from blocking network calls if needed
+
+        BLOCKING_HOOK = ctypes.WINFUNCTYPE(ctypes.wintypes.DWORD)
+
+        self.watchdog_callback = BLOCKING_HOOK(self.socket_timeout_watchdog)
+
+        self.WSASetBlockingHook(self.watchdog_callback)
+
+        # trigger the signal if the execution time is exceeded
+
+        self._original_sigint_handler = signal.getsignal(signal.SIGINT)
+
+        signal.signal(signal.SIGINT, self.handle_death_penalty)
+
+        self.watchdog_thread = _thread.start_new_thread(self.thread_timeout_watchdog, ())
 
     def cancel_death_penalty(self):
-        pass
+        self.wait_for_timeout = False
+
+        self.WSAUnhookBlockingHook()
+        self.CloseHandle(self.timeout_event)
