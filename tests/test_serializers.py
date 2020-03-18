b4e31b49b7a851e6936cad99c3ba09df7c868441
solababs <solababatunde12@gmail.com>
2020-03-18 14:57:02 +0100

    Update tests for better code coverage

--- tests/test_serializers.py
+++ tests/test_serializers.py
@@ -3,12 +3,20 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import json
+import sys
 from rq.serializers import resolve_serializer, MySerializer
+from rq.exceptions import DeserializationError
 
 try:
     import unittest
 except ImportError:
     import unittest2 as unittest  # noqa
+
+is_py2 = sys.version[0] == '2'
+if is_py2:
+    import Queue as queue
+else:
+    import queue as queue
 
 
 class TestSerializers(unittest.TestCase):
@@ -31,3 +39,7 @@ class TestSerializers(unittest.TestCase):
         # Test raise NotImplmentedError
         with self.assertRaises(NotImplementedError):
             resolve_serializer(object)
+
+        # Test raise DeserializationError
+        with self.assertRaises(DeserializationError):
+            serializer.dumps(queue.Queue())
