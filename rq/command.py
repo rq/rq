9ba2d2d5a5cbdafad3ffcc336e71ac58bbae5eaa
Selwin Ong <selwin.ong@gmail.com>
2020-11-14 15:29:22 +0700

    Added send_stop_job_command().

--- rq/command.py
+++ rq/command.py
@@ -1,12 +1,16 @@
 import json
+import os
+import signal
 
 
 PUBSUB_CHANNEL_TEMPLATE = 'rq:pubsub:%s'
 
 
-def send_command(redis, worker_name, command):
+def send_command(redis, worker_name, command, **kwargs):
     """Use Redis' pubsub mechanism to send a command"""
     payload = {'command': command}
+    if kwargs:
+        payload.update(kwargs)
     redis.publish(PUBSUB_CHANNEL_TEMPLATE % worker_name, json.dumps(payload))
 
 
@@ -23,3 +27,45 @@ def send_shutdown_command(redis, worker_name):
 def send_kill_horse_command(redis, worker_name):
     """Tell worker to kill it's horse"""
     send_command(redis, worker_name, 'kill-horse')
+
+
+def send_stop_job_command(redis, worker_name, job_id):
+    """Tell worker to stop a job"""
+    send_command(redis, worker_name, 'stop-job', job_id=job_id)
+
+
+def handle_command(worker, payload):
+    """Parses payload and routes commands"""
+    if payload['command'] == 'stop-job':
+        handle_stop_job_command(worker, payload)
+    elif payload['command'] == 'shutdown':
+        handle_shutdown_command(worker)
+    elif payload['command'] == 'kill-horse':
+        handle_kill_worker_command(worker, payload)
+
+
+def handle_shutdown_command(worker):
+    """Perform shutdown command"""
+    worker.log.info('Received shutdown command, sending SIGINT signal.')
+    pid = os.getpid()
+    os.kill(pid, signal.SIGINT)
+
+
+def handle_kill_worker_command(worker, payload):
+    """Stops work horse"""
+    worker.log.info('Received kill horse command.')
+    if worker.horse_pid:
+        worker.log.info('Kiling horse...')
+        worker.kill_horse()
+    else:
+        worker.log.info('Worker is not working, kill horse command ignored')
+
+
+def handle_stop_job_command(worker, payload):
+    """Handles stop job command"""
+    job_id = payload.get('job_id')
+    worker.log.debug('Received command to stop job %s', job_id)
+    if job_id and worker.get_current_job_id() == job_id:
+        worker.kill_horse()
+    else:
+        worker.log.info('Not working on job %s, command ignored.', job_id)
--- tests/test_commands.py
+++ tests/test_commands.py
@@ -6,7 +6,9 @@ from tests import RQTestCase
 from tests.fixtures import long_running_job
 
 from rq import Queue, Worker
-from rq.command import send_command, send_kill_horse_command, send_shutdown_command
+from rq.command import send_command, send_kill_horse_command, send_shutdown_command, send_stop_job_command
+from rq.job import Job, JobStatus
+from rq.worker import WorkerStatus
 
 
 class TestCommands(RQTestCase):
@@ -43,3 +45,31 @@ class TestCommands(RQTestCase):
         p.join(1)
         job.refresh()
         self.assertTrue(job.id in queue.failed_job_registry)
+
+    def test_stop_job_command(self):
+        """Ensure that stop_job command works properly."""
+
+        connection = self.testconn
+        queue = Queue('foo', connection=connection)
+        job = queue.enqueue(long_running_job, 3)
+        worker = Worker('foo', connection=connection)
+
+        def start_work():
+            worker.work(burst=True)
+
+        p = Process(target=start_work)
+        p.start()
+        p.join(1)
+
+        time.sleep(0.1)
+        send_stop_job_command(connection, worker.name, job_id='1')
+        time.sleep(0.25)
+        # Worker still working due to job_id mismatch
+        worker.refresh()
+        self.assertEqual(worker.get_state(), WorkerStatus.BUSY)
+
+        send_stop_job_command(connection, worker.name, job_id=job.id)
+        time.sleep(0.25)
+        # Worker has stopped working        
+        worker.refresh()
+        self.assertEqual(worker.get_state(), WorkerStatus.IDLE)
