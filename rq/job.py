93a193d2e0cebaf20e61acfaf425086be9f61c60
Steve Kieffer <steve.kieffer@runbox.com>
2021-01-09 18:12:17 -0500

    Check for `dependency_id` in `Job.restore()` for backwd. compat.
    Also eliminate use of `as_text()` here, in favor of `.decode()`.

--- rq/job.py
+++ rq/job.py
@@ -499,7 +499,10 @@ class Job(object):
         self.failure_ttl = int(obj.get('failure_ttl')) if obj.get('failure_ttl') else None  # noqa
         self._status = obj.get('status').decode() if obj.get('status') else None
 
-        self._dependency_ids = json.loads(as_text(obj.get('dependency_ids', '[]')))
+        dep_ids = obj.get('dependency_ids')
+        dep_id = obj.get('dependency_id')  # for backwards compatibility
+        self._dependency_ids = ( json.loads(dep_ids.decode()) if dep_ids
+                                else [dep_id.decode()] if dep_id else [] )
 
         self.ttl = int(obj.get('ttl')) if obj.get('ttl') else None
         self.meta = self.serializer.loads(obj.get('meta')) if obj.get('meta') else {}
