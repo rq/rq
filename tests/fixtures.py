e6993310b04c673a9ad29a15e5d3bd5520eb2301
Steve Kieffer <steve.kieffer@runbox.com>
2021-01-27 17:58:18 -0500

    Improve doctext and formatting.

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -63,7 +63,8 @@ def rpush(key, value, append_worker_name=False, sleep=0):
     which jobs were executed."""
     if sleep:
         time.sleep(sleep)
-    if append_worker_name: value += ':' + get_current_job().worker_name
+    if append_worker_name:
+        value += ':' + get_current_job().worker_name
     redis = get_current_connection()
     redis.rpush(key, value)
 
@@ -235,7 +236,8 @@ def start_worker_process(queue_name, connection=None, worker_name=None, burst=Fa
 def burst_two_workers(queue, timeout=2, tries=5, pause=0.1):
     """
     Get two workers working simultaneously in burst mode, on a given queue.
-    Return after both workers have finished handling jobs.
+    Return after both workers have finished handling jobs, up to a fixed timeout
+    on the worker that runs in another process.
     """
     w1 = start_worker_process(queue.name, worker_name='w1', burst=True)
     w2 = Worker(queue, name='w2')
