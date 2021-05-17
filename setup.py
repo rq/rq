ef4f0611a75ef02e77cb2ad1d4a0a23711ec8827
Selwin Ong <selwin.ong@gmail.com>
2021-05-17 14:00:00 +0700

    Added Python 3.9 to test matrix

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet',
         'Topic :: Scientific/Engineering',
