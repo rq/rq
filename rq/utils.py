33ea1d474f79716c8c7619a2384401db114354a0
Adrian Sadłocha <adrian@sadlocha.eu>
2021-06-22 18:31:25 +0200

    Remove an outdaded comment

--- rq/utils.py
+++ rq/utils.py
@@ -310,7 +310,6 @@ def get_call_string(func_name, args, kwargs):
     arg_list = [as_text(truncate_long_string(repr(arg))) for arg in args]
 
     kwargs = ['{0}={1}'.format(k, as_text(truncate_long_string(repr(v)))) for k, v in kwargs.items()]
-    # Sort here because python 3.3 & 3.4 makes different call_string
     arg_list += sorted(kwargs)
     args = ', '.join(arg_list)
 
