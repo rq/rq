fe85f736860d79effa8689b8c30b30dc4c38717a
rpkak <rpkak@users.noreply.github.com>
2021-07-31 23:19:24 +0200

    use xelatex

--- docssphinx/conf.py
+++ docssphinx/conf.py
@@ -101,3 +101,7 @@ html_context = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# LaTeX (PDF)
+
+latex_engine = 'xelatex'
