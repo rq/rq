c6f8716c8131b3aff02ba4fbd1a366b485fb2af2
Selwin Ong <selwin.ong@gmail.com>
2021-11-06 10:51:57 +0700

    Added test for Result.get_latest()

--- tests/test_results.py
+++ tests/test_results.py
@@ -8,10 +8,14 @@ from .fixtures import say_hello
 
 class TestScheduledJobRegistry(RQTestCase):
 
-    def test_save(self):
+    def test_save_and_get_result(self):
         """Ensure data is saved properly"""
         queue = Queue(connection=self.connection)
         job = queue.enqueue(say_hello)
+
+        result = Result.get_latest(job.id, self.connection)
+        self.assertIsNone(result)
+
         Result.create(job, Result.Type.SUCCESSFUL, ttl=10, return_value=1)
         result = Result.get_latest(job.id, self.connection)
         self.assertEqual(result.return_value, 1)
