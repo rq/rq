c1f9ccd0192482ba57ba76f25ca3066c262b8ee1
Mike Hill <mhilluniversal@gmail.com>
2022-03-09 12:36:13 -0500

    Implemented cross-thread method of raising errors by using ctypes

--- rq/timeouts.py
+++ rq/timeouts.py
@@ -2,6 +2,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import ctypes
 import signal
 import threading
 
@@ -82,27 +83,42 @@ class UnixSignalDeathPenalty(BaseDeathPenalty):
 class TimerDeathPenalty(BaseDeathPenalty):
     def __init__(self, timeout, exception=JobTimeoutException, **kwargs):
         super().__init__(timeout, exception, **kwargs)
+        self._target_thread_id = threading.current_thread().ident
         self._timer = None
-        self.reset_timer()
 
-    def reset_timer(self):
-        """Creates a new timer since timers can only be used once."""
-        self._timer = threading.Timer(self._timeout, self.handle_death_penalty)
+        # Monkey-patch exception with the message ahead of time
+        # since PyThreadState_SetAsyncExc can only take a class
+        def init_with_message(self, *args, **kwargs):  # noqa
+            super(exception, self).__init__(
+                "Task exceeded maximum timeout value ({0} seconds)".format(timeout)
+            )
 
-    def handle_death_penalty(self, signum, frame):
-        raise self._exception(
-            "Task exceeded maximum timeout value ({0} seconds)".format(self._timeout)
+        self._exception.__init__ = init_with_message
+
+    def new_timer(self):
+        """Returns a new timer since timers can only be used once."""
+        return threading.Timer(self._timeout, self.handle_death_penalty)
+
+    def handle_death_penalty(self):
+        """Raises an asynchronous exception in another thread.
+
+        Reference http://docs.python.org/c-api/init.html#PyThreadState_SetAsyncExc for more info.
+        """
+        ret = ctypes.pythonapi.PyThreadState_SetAsyncExc(
+            ctypes.c_long(self._target_thread_id), ctypes.py_object(self._exception)
         )
+        if ret == 0:
+            raise ValueError("Invalid thread ID {}".format(self._target_thread_id))
+        elif ret > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(self._target_thread_id), 0)
+            raise SystemError("PyThreadState_SetAsyncExc failed")
 
     def setup_death_penalty(self):
-        """Sets up an alarm signal and a signal handler that raises
-        an exception after the timeout amount (expressed in seconds).
-        """
+        """Starts the timer."""
+        self._timer = self.new_timer()
         self._timer.start()
 
     def cancel_death_penalty(self):
-        """Removes the death penalty alarm and puts back the system into
-        default signal handling.
-        """
+        """Cancels the timer."""
         self._timer.cancel()
-        self.reset_timer()
+        self._timer = None
