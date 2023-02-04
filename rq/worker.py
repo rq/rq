c8c4b74a3a6f7566409d7083e70cd8bd0ef0a152
Rony Lutsky <ronlut@gmail.com>
2023-02-04 16:55:03 +0200

    Use worker TTL for timeout

--- rq/worker.py
+++ rq/worker.py
@@ -227,7 +227,11 @@ class Worker:
         serializer=None,
     ):  # noqa
 
-        connection = self._set_connection(connection, default_worker_ttl)
+        self.default_result_ttl = default_result_ttl
+        self.default_worker_ttl = default_worker_ttl
+        self.job_monitoring_interval = job_monitoring_interval
+
+        connection = self._set_connection(connection)
         self.connection = connection
         self.redis_server_version = None
 
@@ -249,10 +253,6 @@ class Worker:
         self.validate_queues()
         self._ordered_queues = self.queues[:]
         self._exc_handlers: List[Callable] = []
-
-        self.default_result_ttl = default_result_ttl
-        self.default_worker_ttl = default_worker_ttl
-        self.job_monitoring_interval = job_monitoring_interval
 
         self._state: str = 'starting'
         self._is_horse: bool = False
@@ -300,20 +300,19 @@ class Worker:
         elif exception_handlers is not None:
             self.push_exc_handler(exception_handlers)
 
-    def _set_connection(self, connection: Optional['Redis'], default_worker_ttl: int) -> 'Redis':
+    def _set_connection(self, connection: Optional['Redis']) -> 'Redis':
         """Configures the Redis connection to have a socket timeout.
         This should timouet the connection in case any specific command hangs at any given time (eg. BLPOP).
         If the connection provided already has a `socket_timeout` defined, skips.
 
         Args:
             connection (Optional[Redis]): The Redis Connection.
-            default_worker_ttl (int): The Default Worker TTL
         """
         if connection is None:
             connection = get_current_connection()
         current_socket_timeout = connection.connection_pool.connection_kwargs.get("socket_timeout")
         if current_socket_timeout is None:
-            timeout = self._get_timeout(default_worker_ttl) + 10
+            timeout = self.timeout + 10
             timeout_config = {"socket_timeout": timeout}
             connection.connection_pool.connection_kwargs.update(timeout_config)
         return connection
@@ -365,11 +364,9 @@ class Worker:
         """Returns whether or not this is the worker or the work horse."""
         return self._is_horse
 
-    def _get_timeout(self, worker_ttl: Optional[int] = None) -> int:
-        timeout = DEFAULT_WORKER_TTL
-        if worker_ttl:
-            timeout = worker_ttl
-        return max(1, timeout - 15)
+    @property
+    def timeout(self) -> int:
+        return max(1, self.default_worker_ttl - 15)
 
     def procline(self, message):
         """Changes the current procname for the process.
@@ -684,7 +681,7 @@ class Worker:
                         self.log.info('Worker %s: stopping on request', self.key)
                         break
 
-                    timeout = None if burst else self._get_timeout()
+                    timeout = None if burst else self.timeout
                     result = self.dequeue_job_and_maintain_ttl(timeout)
                     if result is None:
                         if burst:
