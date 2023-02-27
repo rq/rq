ed59b9248aeb87c566661d0f5ee2c2e934c41440
Simó Albert i Beltran <sim6@bona.gent>
2023-02-28 00:03:47 +0100

    fix: Dependency list in depends_on (#1843)
    * test: Dependency list in depends_on
    
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>
    
    * fix: Dependency list in depends_on
    
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>
    
    ---------
    
    Signed-off-by: Simó Albert i Beltran <sim6@bona.gent>

--- rq/job.py
+++ rq/job.py
@@ -254,12 +254,17 @@ class Job:
 
         # dependency could be job instance or id, or iterable thereof
         if depends_on is not None:
-            if isinstance(depends_on, Dependency):
-                job.enqueue_at_front = depends_on.enqueue_at_front
-                job.allow_dependency_failures = depends_on.allow_failure
-                depends_on_list = depends_on.dependencies
-            else:
-                depends_on_list = ensure_list(depends_on)
+            depends_on = ensure_list(depends_on)
+            depends_on_list = []
+            for depends_on_item in depends_on:
+                if isinstance(depends_on_item, Dependency):
+                    # If a Dependency has enqueue_at_front or allow_failure set to True, these behaviors are used for
+                    # all dependencies.
+                    job.enqueue_at_front = job.enqueue_at_front or depends_on_item.enqueue_at_front
+                    job.allow_dependency_failures = job.allow_dependency_failures or depends_on_item.allow_failure
+                    depends_on_list.extend(depends_on_item.dependencies)
+                else:
+                    depends_on_list.extend(ensure_list(depends_on_item))
             job._dependency_ids = [dep.id if isinstance(dep, Job) else dep for dep in depends_on_list]
 
         return job
