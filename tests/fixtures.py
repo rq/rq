2dba00cc1ffff08efc11e1bcfeeaa4b7a6cc3de9
Selwin Ong <selwin.ong@gmail.com>
2023-04-09 19:07:52 +0700

    Fix test

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -212,7 +212,7 @@ class DummyQueue:
     pass
 
 
-def kill_worker(pid, double_kill, interval=0.5):
+def kill_worker(pid: int, double_kill: bool, interval: float = 1.5):
     # wait for the worker to be started over on the main process
     time.sleep(interval)
     os.kill(pid, signal.SIGTERM)
