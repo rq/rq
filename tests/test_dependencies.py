20b32bb5f648683544deb63b817d0743c2bf8abe
Ethan Wolinsky <ethan@alphataraxia.com>
2023-05-15 13:12:16 -0400

    Add tests for enqueue_many with dependencies

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -6,7 +6,6 @@ from rq.job import Job, JobStatus, Dependency
 
 
 class TestDependencies(RQTestCase):
-
     def test_allow_failure_is_persisted(self):
         """Ensure that job.allow_dependency_failures is properly set
         when providing Dependency object to depends_on."""
@@ -70,10 +69,8 @@ class TestDependencies(RQTestCase):
         # When a failing job has multiple dependents, only enqueue those
         # with allow_failure=True
         parent_job = q.enqueue(div_by_zero)
-        job_allow_failure = q.enqueue(say_hello,
-                                      depends_on=Dependency(jobs=parent_job, allow_failure=True))
-        job = q.enqueue(say_hello,
-                        depends_on=Dependency(jobs=parent_job, allow_failure=False))
+        job_allow_failure = q.enqueue(say_hello, depends_on=Dependency(jobs=parent_job, allow_failure=True))
+        job = q.enqueue(say_hello, depends_on=Dependency(jobs=parent_job, allow_failure=False))
         w.work(burst=True, max_jobs=1)
         self.assertEqual(parent_job.get_status(), JobStatus.FAILED)
         self.assertEqual(job_allow_failure.get_status(), JobStatus.QUEUED)
@@ -101,21 +98,150 @@ class TestDependencies(RQTestCase):
         # Test dependant is enqueued at front
         q.empty()
         parent_job = q.enqueue(say_hello)
-        q.enqueue(
-            say_hello,
-            job_id='fake_job_id_1',
-            depends_on=Dependency(jobs=[parent_job])
-        )
-        q.enqueue(
-            say_hello,
-            job_id='fake_job_id_2',
-            depends_on=Dependency(jobs=[parent_job],enqueue_at_front=True)
-        )
-        #q.enqueue(say_hello) # This is a filler job that will act as a separator for jobs, one will be enqueued at front while the other one at the end of the queue
+        q.enqueue(say_hello, job_id='fake_job_id_1', depends_on=Dependency(jobs=[parent_job]))
+        q.enqueue(say_hello, job_id='fake_job_id_2', depends_on=Dependency(jobs=[parent_job], enqueue_at_front=True))
+        # q.enqueue(say_hello) # This is a filler job that will act as a separator for jobs, one will be enqueued at front while the other one at the end of the queue
         w.work(burst=True, max_jobs=1)
 
         self.assertEqual(q.job_ids, ["fake_job_id_2", "fake_job_id_1"])
 
+    def test_multiple_jobs_with_dependencies(self):
+        """Enqueue dependent jobs only when appropriate"""
+        q = Queue(connection=self.testconn)
+        w = SimpleWorker([q], connection=q.connection)
+
+        # enqueue dependent job when parent successfully finishes
+        parent_job = q.enqueue(say_hello)
+        job1 = Queue.prepare_data(say_hello, depends_on=parent_job)
+        job2 = Queue.prepare_data(say_hello, depends_on=parent_job)
+
+        jobs = q.enqueue_many([job1, job2])
+        w.work(burst=True)
+        job1 = Job.fetch(jobs[0].id, connection=self.testconn)
+        job2 = Job.fetch(jobs[1].id, connection=self.testconn)
+        self.assertEqual(job1.get_status(), JobStatus.FINISHED)
+        self.assertEqual(job2.get_status(), JobStatus.FINISHED)
+        q.empty()
+
+        # don't enqueue dependent job when parent fails
+        parent_job = q.enqueue(div_by_zero)
+        job1 = Queue.prepare_data(say_hello, depends_on=parent_job)
+        job2 = Queue.prepare_data(say_hello, depends_on=parent_job)
+
+        jobs = q.enqueue_many([job1, job2])
+        w.work(burst=True)
+        job1 = Job.fetch(jobs[0].id, connection=self.testconn)
+        job2 = Job.fetch(jobs[1].id, connection=self.testconn)
+        self.assertNotEqual(job1.get_status(), JobStatus.FINISHED)
+        self.assertNotEqual(job2.get_status(), JobStatus.FINISHED)
+        q.empty()
+
+        # don't enqueue dependent job when Dependency.allow_failure=False (the default)
+        parent_job = q.enqueue(div_by_zero)
+        dependency = Dependency(jobs=parent_job)
+        job1 = Queue.prepare_data(say_hello, depends_on=dependency)
+        job2 = Queue.prepare_data(say_hello, depends_on=dependency)
+
+        jobs = q.enqueue_many([job1, job2])
+        w.work(burst=True)
+        job1 = Job.fetch(jobs[0].id, connection=self.testconn)
+        job2 = Job.fetch(jobs[1].id, connection=self.testconn)
+        self.assertNotEqual(job1.get_status(), JobStatus.FINISHED)
+        self.assertNotEqual(job2.get_status(), JobStatus.FINISHED)
+
+        # enqueue dependent job when Dependency.allow_failure=True
+        parent_job = q.enqueue(div_by_zero)
+        dependency = Dependency(jobs=parent_job, allow_failure=True)
+        job1 = Queue.prepare_data(say_hello, depends_on=dependency)
+        job2 = Queue.prepare_data(say_hello, depends_on=dependency)
+
+        jobs = q.enqueue_many([job1, job2])
+        w.work(burst=True)
+        job1 = Job.fetch(jobs[0].id, connection=self.testconn)
+        job2 = Job.fetch(jobs[1].id, connection=self.testconn)
+        self.assertEqual(job1.get_status(), JobStatus.FINISHED)
+        self.assertEqual(job2.get_status(), JobStatus.FINISHED)
+
+        # When a failing job has multiple dependents, only enqueue those
+        # with allow_failure=True
+        parent_job = q.enqueue(div_by_zero)
+        dependency_allow_failure = Dependency(jobs=parent_job, allow_failure=True)
+        dependency = Dependency(jobs=parent_job, allow_failure=False)
+
+        job_allow_failure = Queue.prepare_data(say_hello, depends_on=dependency_allow_failure)
+        job = Queue.prepare_data(say_hello, depends_on=dependency)
+        jobs = q.enqueue_many([job_allow_failure, job])
+
+        w.work(burst=True, max_jobs=1)
+        self.assertEqual(parent_job.get_status(), JobStatus.FAILED)
+        self.assertEqual(jobs[0].get_status(), JobStatus.QUEUED)
+        self.assertEqual(jobs[1].get_status(), JobStatus.DEFERRED)
+        q.empty()
+
+        # only enqueue dependent job when all dependencies have finished/failed
+        first_parent_job = q.enqueue(div_by_zero)
+        second_parent_job = q.enqueue(say_hello)
+        dependencies = Dependency(jobs=[first_parent_job, second_parent_job], allow_failure=True)
+        job1 = Queue.prepare_data(say_hello, depends_on=dependencies)
+        job2 = Queue.prepare_data(say_hello, depends_on=dependencies)
+
+        jobs = q.enqueue_many([job1, job2])
+        w.work(burst=True, max_jobs=1)
+        self.assertEqual(first_parent_job.get_status(), JobStatus.FAILED)
+        self.assertEqual(second_parent_job.get_status(), JobStatus.QUEUED)
+        self.assertEqual(jobs[0].get_status(), JobStatus.DEFERRED)
+        self.assertEqual(jobs[1].get_status(), JobStatus.DEFERRED)
+
+        # When second job finishes, dependent job should be queued
+        w.work(burst=True, max_jobs=1)
+        self.assertEqual(second_parent_job.get_status(), JobStatus.FINISHED)
+        self.assertEqual(jobs[0].get_status(), JobStatus.QUEUED)
+        self.assertEqual(jobs[1].get_status(), JobStatus.QUEUED)
+        w.work(burst=True)
+        job1 = Job.fetch(jobs[0].id, connection=self.testconn)
+        job2 = Job.fetch(jobs[1].id, connection=self.testconn)
+        self.assertEqual(job1.get_status(), JobStatus.FINISHED)
+        self.assertEqual(job2.get_status(), JobStatus.FINISHED)
+        q.empty()
+
+        # Multiple jobs are enqueued with correct status
+        parent_job = q.enqueue(say_hello)
+        job_no_deps = Queue.prepare_data(say_hello)
+        job_with_deps = Queue.prepare_data(say_hello, depends_on=parent_job)
+        jobs = q.enqueue_many([job_no_deps, job_with_deps])
+        self.assertEqual(jobs[0].get_status(), JobStatus.QUEUED)
+        self.assertEqual(jobs[1].get_status(), JobStatus.DEFERRED)
+        w.work(burst=True, max_jobs=1)
+        self.assertEqual(jobs[1].get_status(), JobStatus.QUEUED)
+        q.empty()
+
+        # Dependent jobs are enqueued if dependencies are met
+        parent_job = q.enqueue(say_hello)
+        job = Queue.prepare_data(say_hello, depends_on=parent_job)
+        w.work(burst=True, max_jobs=1)
+        jobs = q.enqueue_many([job])
+        job = Job.fetch(jobs[0].id, connection=self.testconn)
+        self.assertEqual(job.get_status(), JobStatus.QUEUED)
+        q.empty()
+
+        # Dependent jobs are deferred if dependencies fail
+        parent_job = q.enqueue(div_by_zero)
+        job = Queue.prepare_data(say_hello, depends_on=parent_job)
+        w.work(burst=True, max_jobs=1)
+        jobs = q.enqueue_many([job])
+        job = Job.fetch(jobs[0].id, connection=self.testconn)
+        self.assertEqual(job.get_status(), JobStatus.DEFERRED)
+        q.empty()
+
+        # Dependent jobs are enqueued if dependencies fail and failures allowed
+        parent_job = q.enqueue(div_by_zero)
+        dependency = Dependency(parent_job, allow_failure=True)
+        job = Queue.prepare_data(say_hello, depends_on=dependency)
+        w.work(burst=True, max_jobs=1)
+        jobs = q.enqueue_many([job])
+        job = Job.fetch(jobs[0].id, connection=self.testconn)
+        self.assertEqual(job.get_status(), JobStatus.QUEUED)
+        q.empty()
 
     def test_dependency_list_in_depends_on(self):
         """Enqueue with Dependency list in depends_on"""
@@ -128,7 +254,6 @@ class TestDependencies(RQTestCase):
         job = q.enqueue_call(say_hello, depends_on=[Dependency([parent_job1]), Dependency([parent_job2])])
         w.work(burst=True)
         self.assertEqual(job.get_status(), JobStatus.FINISHED)
-
 
     def test_enqueue_job_dependency(self):
         """Enqueue via Queue.enqueue_job() with depencency"""
@@ -146,7 +271,6 @@ class TestDependencies(RQTestCase):
         w.work(burst=True)
         self.assertEqual(parent_job.get_status(), JobStatus.FINISHED)
         self.assertEqual(job.get_status(), JobStatus.FINISHED)
-
 
     def test_dependencies_are_met_if_parent_is_canceled(self):
         """When parent job is canceled, it should be treated as failed"""
