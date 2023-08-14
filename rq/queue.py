65cad3d9ee332e5a64d8130ff8ce1683419a569e
Selwin Ong <selwin.ong@gmail.com>
2023-08-14 07:43:29 +0700

    Warning cleanups

--- rq/queue.py
+++ rq/queue.py
@@ -161,7 +161,7 @@ class Queue:
         is_async: bool = True,
         job_class: Optional[Union[str, Type['Job']]] = None,
         serializer: Any = None,
-        death_penalty_class: Type[BaseDeathPenalty] = UnixSignalDeathPenalty,
+        death_penalty_class: Optional[Type[BaseDeathPenalty]] = UnixSignalDeathPenalty,
         **kwargs,
     ):
         """Initializes a Queue object.
@@ -195,7 +195,7 @@ class Queue:
             if isinstance(job_class, str):
                 job_class = import_attribute(job_class)
             self.job_class = job_class
-        self.death_penalty_class = death_penalty_class
+        self.death_penalty_class = death_penalty_class  # type: ignore
 
         self.serializer = resolve_serializer(serializer)
         self.redis_server_version: Optional[Tuple[int, int, int]] = None
@@ -1327,11 +1327,11 @@ class Queue:
         cls,
         queues: List['Queue'],
         timeout: Optional[int],
-        connection: Optional['Redis'] = None,
-        job_class: Optional['Job'] = None,
+        connection: 'Redis',
+        job_class: Optional[Type['Job']] = None,
         serializer: Any = None,
         death_penalty_class: Optional[Type[BaseDeathPenalty]] = None,
-    ) -> Tuple['Job', 'Queue']:
+    ) -> Optional[Tuple['Job', 'Queue']]:
         """Class method returning the job_class instance at the front of the given
         set of Queues, where the order of the queues is important.
 
@@ -1356,7 +1356,7 @@ class Queue:
         Returns:
             job, queue (Tuple[Job, Queue]): A tuple of Job, Queue
         """
-        job_class: Job = backend_class(cls, 'job_class', override=job_class)
+        job_cls: Type[Job] = backend_class(cls, 'job_class', override=job_class)  # type: ignore
 
         while True:
             queue_keys = [q.key for q in queues]
@@ -1370,12 +1370,12 @@ class Queue:
             queue = cls.from_queue_key(
                 queue_key,
                 connection=connection,
-                job_class=job_class,
+                job_class=job_cls,
                 serializer=serializer,
                 death_penalty_class=death_penalty_class,
             )
             try:
-                job = job_class.fetch(job_id, connection=connection, serializer=serializer)
+                job = job_cls.fetch(job_id, connection=connection, serializer=serializer)
             except NoSuchJobError:
                 # Silently pass on jobs that don't exist (anymore),
                 # and continue in the look
