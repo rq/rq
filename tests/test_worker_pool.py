e379151e2cfaa2dfbe278707055494b90b99fb3b
chromium7 <christofer@ui.co.id>
2024-02-12 08:56:41 +0700

    Delay a bit longer for test

--- tests/test_worker_pool.py
+++ tests/test_worker_pool.py
@@ -46,7 +46,7 @@ class TestWorkerPool(TestCase):
         sleep(0.5)
         _send_shutdown_command(worker_data.name, self.connection.connection_pool.connection_kwargs.copy(), delay=0)
         # 1 worker should be dead since we sent a shutdown command
-        sleep(0.2)
+        sleep(0.75)
         pool.check_workers(respawn=False)
         self.assertEqual(len(pool.worker_dict.keys()), 1)
 
@@ -69,7 +69,7 @@ class TestWorkerPool(TestCase):
         sleep(0.5)
         _send_shutdown_command(worker_data.name, self.connection.connection_pool.connection_kwargs.copy(), delay=0)
         # 1 worker should be dead since we sent a shutdown command
-        sleep(0.2)
+        sleep(0.75)
         pool.reap_workers()
         self.assertEqual(len(pool.worker_dict.keys()), 1)
         pool.stop_workers()
