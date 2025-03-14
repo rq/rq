7d265f2f01a040e6b0477b74156e866686ade83e
Isidro <isidroariass@hotmail.es>
2025-03-14 14:23:08 +0100

    Fix example (#2205)
    Failing since 67624eba0b09 (#2043)
    with the following error:
    
    Traceback (most recent call last):
      File "/tmp/rq/examples/run_example.py", line 6, in <module>
        from rq import Connection, Queue
    ImportError: cannot import name 'Connection' from 'rq' (/tmp/rq/rq/__init__.py). Did you mean: 'connections'?

--- examples/run_worker.py
+++ examples/run_worker.py
@@ -1,7 +1,8 @@
-from rq import Connection, Queue, Worker
+from redis import Redis
+from rq import Queue, Worker
 
 if __name__ == '__main__':
     # Tell rq what Redis connection to use
-    with Connection():
-        q = Queue()
+    with Redis() as connection:
+        q = Queue(connection=connection)
         Worker(q).work()
