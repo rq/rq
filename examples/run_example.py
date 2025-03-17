4e0957c4c027a0ce9e9261e90b4cc7738ae02cce
Isidro Arias <isidroariass@hotmail.es>
2025-03-17 20:02:56 +0100

    convert jobs from dict to list

--- examples/run_example.py
+++ examples/run_example.py
@@ -12,10 +12,9 @@ def main(connection):
     fib_range = range(20, 34)
 
     # Kick off the tasks asynchronously
-    jobs = {}
     q = Queue(connection=connection)
-    for x in fib_range:
-        jobs[x] = q.enqueue(slow_fib, x)
+    jobs = [q.enqueue(slow_fib, arg) for arg in fib_range]
+
 
     start_time = time.time()
     done = False
@@ -23,14 +22,14 @@ def main(connection):
         os.system('clear')
         print('Asynchronously: (now = %.2f)' % (time.time() - start_time,))
         done = True
-        for x in fib_range:
-            job = jobs[x]
+        for job in jobs:
             result = job.return_value()
             if result is None:
                 done = False
                 status = job.get_status(refresh=False)
                 result = '(%s)' % status.name
-            print('fib(%d) = %s' % (x, result))
+            (arg,) = job.args
+            print('fib(%d) = %s' % (arg, result))
         print('')
         print('To start the actual in the background, run a worker:')
         print('    $ rq worker')
