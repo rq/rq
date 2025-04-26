3089a61583e74125fcf90a7b910f438906510369
Selwin Ong <selwin.ong@gmail.com>
2025-04-26 10:27:16 +0700

    Move worker and worker_pool CLI commands to a separate file

--- rq/cli/__init__.py
+++ rq/cli/__init__.py
@@ -3,4 +3,5 @@ from .cli import main
 
 # TODO: the following imports can be removed when we drop the `rqinfo` and
 # `rqworkers` commands in favor of just shipping the `rq` command.
-from .cli import info, worker
+from .cli import info
+from .workers import worker
