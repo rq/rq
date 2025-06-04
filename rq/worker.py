02f1cafbf9a7c08f4bb9ed0e484b9a06b4dc3cb2
Nicolas <nicolas.jankovic1@gmail.com>
2025-06-04 15:42:29 +0200

    Update python versions set in the project (#2250)
    * - Removed depreciated python versions
    - When latest depreciated was specified, the soon to be depreciated replaces it
    
    * - Add missing recent python versions in CI/CD and tests
    - Fix mypy error when importing signal value by shadowing the previous import in try except condition
    
    * Handle SIGKILL and SIGTERM availability as module level variable instead of default param in function
    
    * Renamed module level variable SIGNAL_SHUTDOWN into SHUTDOWN_SIGNAL
    
    * Fixed lint issue when renaming variable
    
    ---------
    
    Co-authored-by: nicolasj <nicolas.jankovic@dataimpact.io>

--- rq/worker.py
+++ rq/worker.py
@@ -25,11 +25,6 @@ if TYPE_CHECKING:
     from redis import Redis
     from redis.client import Pipeline, PubSub, PubSubWorkerThread
 
-try:
-    from signal import SIGKILL
-except ImportError:
-    from signal import SIGTERM as SIGKILL  # type: ignore[assignment]
-
 from contextlib import suppress
 
 import redis.exceptions
@@ -102,6 +97,7 @@ def signal_name(signum):
     except ValueError:
         return 'SIG_UNKNOWN'
 
+SHUTDOWN_SIGNAL = signal.SIGTERM if not hasattr(signal, 'SIGKILL') else signal.SIGKILL
 
 class DequeueStrategy(str, Enum):
     DEFAULT = 'default'
@@ -1231,7 +1227,7 @@ class BaseWorker:
         """Pushes an exception handler onto the exc handler stack."""
         self._exc_handlers.append(handler_func)
 
-    def kill_horse(self, sig: signal.Signals = SIGKILL):
+    def kill_horse(self, sig: signal.Signals = SHUTDOWN_SIGNAL):
         raise NotImplementedError()
 
 
@@ -1241,7 +1237,7 @@ class Worker(BaseWorker):
         """Returns whether or not this is the worker or the work horse."""
         return self._is_horse
 
-    def kill_horse(self, sig: signal.Signals = SIGKILL):
+    def kill_horse(self, sig: signal.Signals = SHUTDOWN_SIGNAL):
         """Kill the horse but catch "No such process" error has the horse could already be dead.
 
         Args:
