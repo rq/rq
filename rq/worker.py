3943ea88ee68a14e8c82edb61bab29e766a359b3
nicolasj <nicolas.jankovic@dataimpact.io>
2025-06-04 15:20:49 +0200

    Renamed module level variable SIGNAL_SHUTDOWN into SHUTDOWN_SIGNAL

--- rq/worker.py
+++ rq/worker.py
@@ -97,7 +97,7 @@ def signal_name(signum):
     except ValueError:
         return 'SIG_UNKNOWN'
 
-SIGNAL_SHUTDOWN = signal.SIGTERM if not hasattr(signal, 'SIGKILL') else signal.SIGKILL
+SHUTDOWN_SIGNAL = signal.SIGTERM if not hasattr(signal, 'SIGKILL') else signal.SIGKILL
 
 class DequeueStrategy(str, Enum):
     DEFAULT = 'default'
