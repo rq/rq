b732132645e0c992e673af128a7324d861e041db
Selwin Ong <selwin.ong@gmail.com>
2025-08-26 11:12:32 +0700

    Refactor dependency processing in `Job.create()`

--- rq/job.py
+++ rq/job.py
@@ -363,21 +363,9 @@ class Job:
         job.meta = meta or {}
         job.group_id = group_id
 
-        # dependency could be job instance or id, or iterable thereof
+        # Process job dependencies
         if depends_on is not None:
-            depends_on_list: list[Union['Job', str]] = []
-            for depends_on_item in ensure_job_list(depends_on):
-                if isinstance(depends_on_item, Dependency):
-                    # If a Dependency has enqueue_at_front or allow_failure set to True, these behaviors are used for
-                    # all dependencies.
-                    job.enqueue_at_front = job.enqueue_at_front or depends_on_item.enqueue_at_front
-                    job.allow_dependency_failures = job.allow_dependency_failures or depends_on_item.allow_failure
-                    depends_on_list.extend(list(depends_on_item.dependencies))
-                else:
-                    # After checking for Dependency, depends_on_item should be Job or str
-                    # Use type cast to inform mypy of the narrowed type
-                    depends_on_list.append(depends_on_item)  # type: ignore[arg-type]
-            job._dependency_ids = [dep.id if isinstance(dep, Job) else dep for dep in depends_on_list]
+            job.process_dependencies(depends_on)
 
         # Set status: explicit status takes precedence, otherwise DEFERRED if has dependencies, CREATED if not
         if status is not None:
@@ -1350,7 +1338,29 @@ class Job:
             assert self is _job_stack.pop()
         return self._result
 
-    def prepare_for_execution(self, worker_name: str, pipeline: 'Pipeline'):
+    def process_dependencies(self, depends_on: JobDependencyType) -> None:
+        """Process job dependencies and set dependency-related attributes.
+
+        Args:
+            depends_on: Job dependencies - can be a Dependency, Job, string ID,
+                       or iterable of these types.
+        """
+
+        depends_on_list: list[Union['Job', str]] = []
+        for depends_on_item in ensure_job_list(depends_on):
+            if isinstance(depends_on_item, Dependency):
+                # If a Dependency has enqueue_at_front or allow_failure set to True, these behaviors are used for
+                # all dependencies.
+                self.enqueue_at_front = self.enqueue_at_front or depends_on_item.enqueue_at_front
+                self.allow_dependency_failures = self.allow_dependency_failures or depends_on_item.allow_failure
+                depends_on_list.extend(list(depends_on_item.dependencies))
+            else:
+                # After checking for Dependency, depends_on_item should be Job or str
+                # Use type cast to inform mypy of the narrowed type
+                depends_on_list.append(depends_on_item)  # type: ignore[arg-type]
+        self._dependency_ids = [dep.id if isinstance(dep, Job) else dep for dep in depends_on_list]
+
+    def prepare_for_execution(self, worker_name: str, pipeline: 'Pipeline') -> None:
         """Prepares the job for execution, setting the worker name,
         heartbeat information, status and other metadata before execution begins.
 
